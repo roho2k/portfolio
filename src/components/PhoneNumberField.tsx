c2313e71bf5bdea57613fe10ec0764a0987e6033
Rodney Ho <rodneyrhkk@gmail.com>
2024-09-12 13:28:39 -0700

    remove handleInputChangePartOne

--- src/components/PhoneNumberField.tsx
+++ src/components/PhoneNumberField.tsx
@@ -11,51 +11,13 @@ export default function PhoneNumberField() {
 		setPhoneNumber(formattedPhoneNumber);
 	};
 
-	const handleInputChangePartOne = (
-		e: React.ChangeEvent<HTMLInputElement>
-	) => {
-		const input = e.target.value;
-		const isDeleting = phoneNumber && phoneNumber.length > input.length;
-		if (isDeleting) {
-			setPhoneNumber(input);
-			return;
-		}
-
-		const parsedInput = input.replace(/\D/g, '');
-		const maxLength = 10;
-
-		let formattedPhoneNumber = parsedInput;
-		if (parsedInput.length >= maxLength) {
-			formattedPhoneNumber =
-				'(' +
-				parsedInput.slice(0, 3) +
-				') ' +
-				parsedInput.slice(3, 6) +
-				'-' +
-				parsedInput.slice(6, maxLength);
-		} else if (parsedInput.length >= 6 && parsedInput.length < maxLength) {
-			formattedPhoneNumber =
-				'(' +
-				parsedInput.slice(0, 3) +
-				') ' +
-				parsedInput.slice(3, 6) +
-				'-' +
-				parsedInput.slice(6);
-		} else if (parsedInput.length >= 3 && parsedInput.length < 6) {
-			formattedPhoneNumber =
-				'(' + parsedInput.slice(0, 3) + ') ' + parsedInput.slice(3);
-		}
-		setPhoneNumber(formattedPhoneNumber);
-		return;
-	};
-
 	return (
 		<div className='flex flex-col items-center p-48 rounded-lg border-2'>
 			<h1 className='text-4xl font-bold'>Phone Number</h1>
 			<input
 				className='border-2 rounded w-60 text-xl font-semibold text-center'
 				type='text'
-				onChange={handleInputChangePartOne}
+				onChange={handleInputChange}
 				value={phoneNumber}
 			/>
 		</div>
